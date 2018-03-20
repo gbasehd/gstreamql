0e493dc6b96eebf78aac614b3284fac3d74fb0aa
mathildaMa <34359704+mathildaMa@users.noreply.github.com>
2018-03-20 11:26:39 +0800

    增加debug参数设置

--- script/flink-prepare.sh
+++ script/flink-prepare.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 hdfs dfs -rm -f "/streamingPro/$1"
 hdfs dfs -put "$1" /streamingPro
-rm /home/mjw/streamingpro/output -f
+if [ $2 = "false" ]
+then
+    rm $1 -f
+fi
 hdfs dfs -ls /streamingPro
