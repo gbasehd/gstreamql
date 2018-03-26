2755b5e5910ff01fbd6551f61c78f47f6b0c9541
mathildaMa <34359704+mathildaMa@users.noreply.github.com>
2018-03-26 11:39:16 +0800

    提交测试代码

--- HiveHook/src/test/com/gbase/streamql/hive/StreamQLParserTest.java
+++ HiveHook/src/test/com/gbase/streamql/hive/StreamQLParserTest.java
@@ -1,8 +1,12 @@
 package com.gbase.streamql.hive;
 
+import org.apache.hadoop.hive.ql.parse.ASTNode;
+import org.apache.hadoop.hive.ql.parse.ParseDriver;
+import org.apache.hadoop.hive.ql.parse.ParseException;
 import org.apache.hadoop.hive.ql.session.SessionState;
 import org.easymock.EasyMock;
 import org.junit.Test;
+import org.stringtemplate.v4.ST;
 
 import java.util.HashMap;
 
@@ -101,7 +105,55 @@ public class StreamQLParserTest {
         String sql = "mjw) bb) (tok_tabref (tok_tabname mjw cc))))";
         String sql2 = "(tok_from (tok_join (tok_tabref (tok_tabname mjw) bb) (tok_tabref (tok_tabname mjw cc))))";
         StringBuilder builder = new StringBuilder();
-        createSqlParser.getTableList(sql, builder);
+        Utility.getTableList(sql, builder);
         System.out.print(builder.toString());
+    }
+
+    @Test
+    public void testParser() throws ParseException {
+        ParseDriver driver = new ParseDriver();
+
+        String wrongSql = "insert into aaa";
+        //driver.parse(wrongSql);
+
+        String insertSql = "insert into bb select count(f0) from aa group by HOP(userActionTime,interval '3' second,interval '6' second)";
+        ASTNode insert = (ASTNode) driver.parse(insertSql).getChild(0);
+        StringBuilder inputStreams = new StringBuilder();
+        StringBuilder outputStreams = new StringBuilder();
+        Utility.getTableList(insert.getChild(0).toStringTree(), inputStreams);
+        Utility.getTableList(insert.getChild(1).toStringTree(), outputStreams);
+        assertEquals(inputStreams.toString(), "aa,");
+        assertEquals(outputStreams.toString(), "bb,");
+
+
+        String insertSql2 = "insert into bb select count(f0) from test group by TUMBLE(userActionTime,interval '3' second)";
+        ASTNode insert2 = (ASTNode) driver.parse(insertSql2).getChild(0);
+        StringBuilder inputStreams2 = new StringBuilder();
+        StringBuilder outputStreams2 = new StringBuilder();
+        Utility.getTableList(insert.getChild(0).toStringTree(), inputStreams2);
+        Utility.getTableList(insert.getChild(1).toStringTree(), outputStreams2);
+        assertEquals(inputStreams2.toString(), "aa,");
+        assertEquals(outputStreams2.toString(), "bb,");
+
+        String insertSql3 = "insert into bb SELECT *,COUNT(f0) OVER (PARTITION BY f0 ORDER BY userActionTime ROWS BETWEEN 200 PRECEDING AND CURRENT ROW) FROM test";
+        ASTNode insert3 = (ASTNode) driver.parse(insertSql3).getChild(0);
+        StringBuilder inputStreams3 = new StringBuilder();
+        StringBuilder outputStreams3 = new StringBuilder();
+        Utility.getTableList(insert.getChild(0).toStringTree(), inputStreams3);
+        Utility.getTableList(insert.getChild(1).toStringTree(), outputStreams3);
+        assertEquals(inputStreams3.toString(), "aa,");
+        assertEquals(outputStreams3.toString(), "bb,");
+
+        String selectSql = "select * from a";
+        driver.parse(selectSql);
+
+        String expr = "a+b";
+        driver.parseExpression(expr);
+
+        String expr2 = "abs(1)";
+        driver.parseExpression(expr2);
+
+        //driver.parse(expr);
+
     }
 }
